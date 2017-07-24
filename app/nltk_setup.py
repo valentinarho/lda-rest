ebe242a19ba3f9fc02a3cd1fa77af5278d65c105
Valentina Rho <rho.valentina@gmail.com>
2017-07-24 15:22:00 +0200

    Fix issue in nltk setup

--- app/nltk_setup.py
+++ app/nltk_setup.py
@@ -5,15 +5,16 @@ from nltk.corpus import stopwords
 
 try:
     wordnet.words
-except LookupError:
-    nltk.download('wordnet')
+except Exception:
+    print('Downloading wordnet.')
+    nltk.download('wordnet', download_dir='/usr/share/nltk_data')
 
-try:
-    punkt.REASON_DEFAULT_DECISION
-except LookupError:
-    nltk.download('punkt')
+# always downloading punkt
+print('Downloading punkt.')
+nltk.download('punkt', download_dir='/usr/share/nltk_data')
 
 try:
     stopwords.words
-except LookupError:
-    nltk.download('stopwords')
+except Exception:
+    print('Downloading stopwords.')
+    nltk.download('stopwords', download_dir='/usr/share/nltk_data')
