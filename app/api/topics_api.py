b81b94cfd6712f54b98da7f17a9e966afad2ed95
Valentina Rho <rho.valentina@gmail.com>
2017-11-01 10:07:47 +0000

    Allowed get instead of only search for topics

--- app/api/topics_api.py
+++ app/api/topics_api.py
@@ -10,26 +10,48 @@ class Topics(Resource):
 
     def get(self, model_id):
         """
-        Retrieve all topics associated to model_id or associated to a specified query
+        Retrieve all topics associated to model_id or associated to a specified query if `text` is specified
 
         :param model_id:
         :return:
         """
         parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument('threshold', default=0.0, required=False, type=float,
+                            help='The minimum probability that a topic should have to be returned as related to the query string.')
+        parser.add_argument('text', default=None, required=False, type=str,
+                            help='The query to assign topics to.')
+
         args = parser.parse_args()
 
-        # else restituisci la lista di topics
-        data = {'topics': db_utils.get_all_topics(model_id), 'model_id': model_id}
+        if args['text'].strip() is not None:
+            data = {'model_id': model_id, 'threshold': args['threshold'], 'textual_query': args['text']}
 
-        # data = api_utils.filter_only_exposed(data, config.exposed_fields['topics'])
-        response = "Topics retrieved."
-        response_code = 200
-        marshalled = marshal(data, api_utils.topics_fields)
+            topics_assignment = lda_utils.assign_topics_for_query(model_id, args['text'], args['threshold'])
+            if topics_assignment is None:
+                response = "Error during topic extraction. Check logs."
+                response_code = 500
+            else:
+                list_of_da = lda_utils.convert_topic_assignment_to_dictionary(topics_assignment)
+                data['assigned_topics'] = sorted(list_of_da[0]['assigned_topics'], reverse=True,
+                                                 key=lambda d: d['topic_weight'])
+                response_code = 200
+                response = "Topics extracted."
+
+            marshalled = marshal(data, api_utils.textual_query_fields)
+        else:
+            # else restituisci la lista di topics
+            data = {'topics': db_utils.get_all_topics(model_id), 'model_id': model_id}
+
+            # data = api_utils.filter_only_exposed(data, config.exposed_fields['topics'])
+            response = "Topics retrieved."
+            response_code = 200
+            marshalled = marshal(data, api_utils.topics_fields)
 
         if response_code == 200:
             return api_utils.prepare_success_response(response_code, response, marshalled)
         else:
             return api_utils.prepare_error_response(response_code, response, marshalled)
+
 
     def search(self, model_id):
         """
